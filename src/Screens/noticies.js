6a17d5dfd64efa7fa8aa3fe901e880c3b297cd2c
QuimTorra <q.torra@gmail.com>
2022-12-17 00:54:35 +0100

    Base Pantalles📲 - chatbot.js - info.js - noticies.js

---
+++ src/Screens/noticies.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const noticies = () => {
+  return (
+    <View>
+      <Text>noticies</Text>
+    </View>
+  );
+};
+
+export default noticies;
+
+const styles = StyleSheet.create({});
