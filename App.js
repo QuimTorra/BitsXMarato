f90e8f1af96e34e2a94b623cc470dab5d7d3b0d7
marc.castellana <marc.castellana@est.fib.upc.edu>
2022-12-16 22:14:05 +0100

    lastchanges presopar

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+
+import landingpage from "./Screens/landingpage.js"
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="landingpage"
+          screenOptions={{
+            headerShown: false,
+          }}
+      >
+          <Stack.Screen name = "landingpage" component = {landingpage}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
