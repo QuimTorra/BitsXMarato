ebeddb3c90df62aa288b408e96aae967403dd79e
PepNav <106391948+PepNav@users.noreply.github.com>
2022-12-16 19:31:11 +0100

    First Screen :D

---
+++ src/Screens/landingpage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function landingpage() {
+  return (
+    <View>
+      <Text>landingpage</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
