58e1f4b00571b9c7f0d2b6da6bd068ea9e64527b
PepNav <106391948+PepNav@users.noreply.github.com>
2022-12-16 19:41:26 +0100

    gitignore and rnpaper

--- src/Screens/landingpage.js
+++ src/Screens/landingpage.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function landingpage() {
   return (
     <View>
-      <Text>landingpage</Text>
+      <Text></Text>
     </View>
   );
 }
