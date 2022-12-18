6389d90812e2c99fda3e49e778521567203e8a71
marc.castellana <marc.castellana@est.fib.upc.edu>
2022-12-18 03:39:36 +0100

    dataset game

--- src/Components/game/datagame.js
+++ src/Components/game/datagame.js
@@ -22,5 +22,53 @@ export const data1 = [
   },
 ];
 
-export const data2 = [{}];
-export const data3 = [{}];
+export const data2 = [
+  {
+    pregunta1: "Quin d'aquests no és un factor de risc?",
+    answer1: "Tractaments hormonals",
+    answer2: "Edad avançada",
+    answer3: "Exposició a tòxics",
+    correct: "3",
+  },
+  {
+    pregunta2: "A què fan referència les sigles TVP",
+    answer1: "TVP, Tromboembolisme Vascular Parcial",
+    answer2: "TVP, Trombosi Venosa Profunda",
+    answer3: "TVP, Trombosi Venar Percutora",
+    correct: "2",
+  },
+  {
+    pregunta3:
+      "Quin percentatge de predisposició a la malaltia es pot atribuïr a factors genètics?",
+    answer1: "Un 20%",
+    answer2: "Un 40%",
+    answer3: "Un 60%",
+    correct: "3",
+  },
+];
+export const data3 = [
+  {
+    pregunta1:
+      "En quina posició de causes de mort al mon ubicaries a la trombosi venosa?",
+    answer1: "Passades les cinquanta",
+    answer2: "Primera posició",
+    answer3: "Entre les vint primeres",
+    correct: "2",
+  },
+  {
+    pregunta1:
+      "Quins d'aquests són factor d'entorns que afecten al risc de patir malalties tromboembòliques?",
+    answer1: "Cirugia general, Enguixament, Edat",
+    answer2: "Tabaquisme, Picades d'alguns animals, Dieta inadequada",
+    answer3: "Estrés, Sedentarisme, Ferides",
+    correct: "1",
+  },
+  {
+    pregunta1:
+      "Després del tractament a la trombosi, fins quin percentatge dels pacients tornaràn a patir un event trombòtic",
+    answer1: "Un 30%",
+    answer2: "Un 50%",
+    answer3: "Un 60%",
+    correct: "1",
+  },
+];
