c99c7dd3698decb431b68967af033318da7058e1
marc.castellana <marc.castellana@est.fib.upc.edu>
2022-12-18 10:58:13 +0100

    text donació, redirect link

--- src/Screens/landingpage.js
+++ src/Screens/landingpage.js
@@ -1,14 +1,32 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableWithoutFeedback,
+  Linking,
+} from "react-native";
 
 import Header from "../Components/landingPage/Header.js";
 import Menu from "../Components/landingPage/Menu.js";
 
 export default function Landingpage({ navigation }) {
+  open = () => {
+    let url = "http://www.wikipedia.org";
+    Linking.openURL(url);
+  };
+
   return (
     <View>
       <Header />
       <Menu navigation={navigation} />
+      <View style={styles.container}>
+        <TouchableWithoutFeedback onPress={this.open}>
+          <View style={styles.viewForText}>
+            <Text> donació </Text>
+          </View>
+        </TouchableWithoutFeedback>
+      </View>
     </View>
   );
 }
